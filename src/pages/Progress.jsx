640e03c3edc65d39c2d780875d74b970c31170f5
24ERIC <60751655+24ERIC@users.noreply.github.com>
2023-05-20 15:50:21 -0400

    add router in react website, add four pages

---
+++ src/pages/Progress.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Progress() {
+  return (
+    <div>
+      <h1>Progress Page</h1>
+      {/* Add your progress page content here */}
+    </div>
+  );
+}
+
+export default Progress;
