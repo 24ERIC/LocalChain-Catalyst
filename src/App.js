fe98defb85274f887bb82f678b6b264ab9dad78d
Roselyn Huynh <roselynh100@gmail.com>
2023-05-20 19:01:15 -0400

    Add wallet buttons, remove unused components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react"
 import * as ReactDOM from "react-dom/client"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { AppShell, Navbar, Header } from "@mantine/core"
+import { AppShell, Navbar, Header, Button } from "@mantine/core"
 import "./App.css"
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
 import Landing from "./pages/Landing"
 import Home from "./pages/Home"
 import Proposals from "./pages/Proposals"
@@ -15,21 +13,21 @@ import Navigation from "./components/Navigation"
 import ScrollingHeader from "./components/ScrollingHeader"
 
 function App() {
-  // function openSignupPopup() {
-  //   document.getElementById("signUpPopup").style.display = "block"
-  // }
+  function openNearAuthPopup() {
+    document.getElementById("nearPopup").style.display = "block"
+  }
 
-  // function closeSignupPopup() {
-  //   document.getElementById("signUpPopup").style.display = "none"
-  // }
+  function openMetamaskAuthPopup() {
+    document.getElementById("metamaskPopup").style.display = "block"
+  }
 
-  // function openLoginPopup() {
-  //   document.getElementById("logInPopup").style.display = "block"
-  // }
+  function closeNearAuthPopup() {
+    document.getElementById("nearPopup").style.display = "none"
+  }
 
-  // function closeLoginPopup() {
-  //   document.getElementById("logInPopup").style.display = "none"
-  // }
+  function closeMetamaskAuthPopup() {
+    document.getElementById("metamaskPopup").style.display = "none"
+  }
 
   // function onClick(element) {
   //   document.getElementById("img01").src = element.src
@@ -38,87 +36,78 @@ function App() {
   //   captionText.innerHTML = element.alt
   // }
 
-  // // Toggle between showing and hiding the sidebar when clicking the menu icon
-  // var mySidebar = document.getElementById("mySidebar")
-
-  // function w3_open() {
-  //   if (mySidebar.style.display === "block") {
-  //     mySidebar.style.display = "none"
-  //   } else {
-  //     mySidebar.style.display = "block"
-  //   }
-  // }
-
-  // // Close the sidebar with the close button
-  // function w3_close() {
-  //   mySidebar.style.display = "none"
-  // }
-
   return (
-    <Router>
-      <AppShell
-        padding="md"
-        navbar={
-          <Navbar width={{ base: 300 }} height={"100%"} p="xs">
-            <Navigation />
-          </Navbar>
-        }
-        header={
-          <Header height={100} p="xs">
-            <ScrollingHeader />
-          </Header>
-        }
-        styles={(theme) => ({
-          main: {
-            backgroundColor: theme.colors.dark[7]
+    <>
+      <Router>
+        <AppShell
+          padding="md"
+          navbar={
+            <Navbar width={{ base: 300 }} height={"100%"} p="xs">
+              <Navigation />
+              <Button onClick={openNearAuthPopup} sx={{ marginBottom: "20px" }}>
+                Connect a NEAR wallet
+              </Button>
+              <Button onClick={openMetamaskAuthPopup}>
+                Connect a MetaMask wallet
+              </Button>
+            </Navbar>
           }
-        })}
-      >
-        <Routes>
-          <Route exact path="/" element={<Landing />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/proposals" element={<Proposals />} />
-          <Route path="/voting" element={<Voting />} />
-          <Route path="/milestones" element={<Milestones />} />
-          <Route path="/submitProposal" element={<SubmitProposal />} />
-        </Routes>
-      </AppShell>
-    </Router>
-    /* <div id="signUpPopup" className="popup">
-          <div className="popup-content">
-            <span className="close" onClick={closeSignupPopup}>
-              &times;
-            </span>
-            <h2>Sign Up</h2>
-            <form>
-              <div className="input-wrapper">
-                <input type="text" placeholder="Username" />
-              </div>
-              <div className="input-wrapper">
-                <input type="password" placeholder="Password" />
-              </div>
-              <button type="submit">Submit</button>
-            </form>
-          </div>
+          header={
+            <Header height={100} p="xs">
+              <ScrollingHeader />
+            </Header>
+          }
+          styles={(theme) => ({
+            main: {
+              backgroundColor: theme.colors.dark[7]
+            }
+          })}
+        >
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/proposals" element={<Proposals />} />
+            <Route path="/voting" element={<Voting />} />
+            <Route path="/milestones" element={<Milestones />} />
+            <Route path="/submitProposal" element={<SubmitProposal />} />
+          </Routes>
+        </AppShell>
+      </Router>
+      <div id="nearPopup" className="popup">
+        <div className="popup-content">
+          <span className="close" onClick={closeNearAuthPopup}>
+            &times;
+          </span>
+          <h2>Connect Near Wallet</h2>
+          <form>
+            <div className="input-wrapper">
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="input-wrapper">
+              <input type="password" placeholder="Password" />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
         </div>
-        <div id="logInPopup" className="popup">
-          <div className="popup-content">
-            <span className="close" onClick={closeLoginPopup}>
-              &times;
-            </span>
-            <h2>Log In</h2>
-            <form>
-              <div className="input-wrapper">
-                <input type="text" placeholder="Username" />
-              </div>
-              <div className="input-wrapper">
-                <input type="password" placeholder="Password" />
-              </div>
-              <button type="submit">Submit</button>
-            </form>
-          </div>
+      </div>
+      <div id="metamaskPopup" className="popup">
+        <div className="popup-content">
+          <span className="close" onClick={closeMetamaskAuthPopup}>
+            &times;
+          </span>
+          <h2>Connect MetaMask Wallet</h2>
+          <form>
+            <div className="input-wrapper">
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="input-wrapper">
+              <input type="password" placeholder="Password" />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
         </div>
-      </div> */
+      </div>
+    </>
   )
 }
 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -12,43 +12,6 @@ const Landing = () => {
       <Header />
       <About />
       <Promo />
-      <nav
-        className="w3-sidebar w3-bar-block w3-black w3-card w3-animate-left w3-hide-medium w3-hide-large"
-        style={{ display: "none" }}
-        id="mySidebar"
-      >
-        <a
-          href="javascript:void(0)"
-          onClick="w3_close()"
-          className="w3-bar-item w3-button w3-large w3-padding-16"
-        >
-          Close ×
-        </a>
-        <a href="#about" onClick="w3_close()" className="w3-bar-item w3-button">
-          ABOUT
-        </a>
-        <a href="#team" onClick="w3_close()" className="w3-bar-item w3-button">
-          TEAM
-        </a>
-        <a href="#work" onClick="w3_close()" className="w3-bar-item w3-button">
-          WORK
-        </a>
-        <a
-          href="#pricing"
-          onClick="w3_close()"
-          className="w3-bar-item w3-button"
-        >
-          PRICING
-        </a>
-        <a
-          href="#contact"
-          onClick="w3_close()"
-          className="w3-bar-item w3-button"
-        >
-          CONTACT
-        </a>
-      </nav>
-
       {/* <!-- Team Section --> */}
       <div className="w3-container" style={{ padding: "128px 16px" }} id="team">
         <h3 className="w3-center">THE TEAM</h3>
