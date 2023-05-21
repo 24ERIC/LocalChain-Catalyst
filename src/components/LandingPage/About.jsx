758cb03c832795ba606c80f87e1e85504501b9b7
24ERIC <60751655+24ERIC@users.noreply.github.com>
2023-05-21 09:56:37 -0400

    finsih

--- src/components/LandingPage/About.jsx
+++ src/components/LandingPage/About.jsx
@@ -1,35 +1,31 @@
 const About = () => {
   return (
     <div className="w3-container" style={{ padding: "128px 16px" }} id="about">
-      <h3 className="w3-center">ABOUT THE COMPANY</h3>
-      <p className="w3-center w3-large">Key features of our company</p>
+      <h3 className="w3-center">ABOUT THE LocalChain Catalyst</h3>
+      <p className="w3-center w3-large">Key features of LocalChain Catalyst</p>
       <div className="w3-row-padding w3-center" style={{ marginTop: "64px" }}>
         <div className="w3-quarter">
           <p className="w3-large">Responsive</p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore.
+          LocalChain Catalyst very responsive to the proposal and voting system.
           </p>
         </div>
         <div className="w3-quarter">
           <p className="w3-large">Passion</p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore.
+          LocalChain Catalyst is passion about the goal we targeting at.
           </p>
         </div>
         <div className="w3-quarter">
           <p className="w3-large">Design</p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore.
+          LocalChain Catalyst design best for you and others.
           </p>
         </div>
         <div className="w3-quarter">
           <p className="w3-large">Support</p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore.
+          LocalChain Catalyst support whole community.
           </p>
         </div>
       </div>
--- src/components/LandingPage/Header.jsx
+++ src/components/LandingPage/Header.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Button, Image, Text, Title } from "@mantine/core"
 
 const Header = () => {
@@ -17,6 +18,37 @@ const Header = () => {
         <Button variant="outline" size="md">
           Submit a proposal
         </Button>
+=======
+
+
+
+const Header = () => {
+  return (
+    <header className="bgimg-1 w3-display-container w3-grayscale-min" id="home">
+      <div
+        className="w3-display-left w3-text-white"
+        style={{ padding: "48px" }}
+      >
+        <span className="w3-jumbo w3-hide-small">
+            You Have 10 Proposals to Vote
+        </span>
+        <br />
+        <span className="w3-xxlarge w3-hide-large w3-hide-medium">
+          Check out the Milestones for approved proposals
+        </span>
+        <br />
+        <span className="w3-large">
+          Check out the Milestones for approved proposals
+        </span>
+        <p>
+          <a
+            href="#about"
+            className="w3-button w3-white w3-padding-large w3-large w3-margin-top w3-opacity w3-hover-opacity-off"
+          >
+            Learn more and start today
+          </a>
+        </p>
+>>>>>>> Stashed changes
       </div>
     </div>
   )
